f86b71b0e7d957b1d55bfbb43ff8ed635a9a42ab
ramya-251103 <134849283+ramya-251103@users.noreply.github.com>
2024-06-05 00:29:07 +0530

    Create IntroductiontoTrees.cpp

---
+++ Day 1/IntroductiontoTrees.cpp
@@ -0,0 +1,17 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int countNodes(int i) {
+        // your code here
+        
+     
+       
+       int max_nodes;
+       
+       max_nodes = pow(2,(i-1));
+       
+       return max_nodes;
+       
+    }
+};
