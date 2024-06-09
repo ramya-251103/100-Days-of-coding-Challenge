271d7abbbac84ab2ff2055ecc1323892cf9d084a
ramya-251103 <134849283+ramya-251103@users.noreply.github.com>
2024-06-09 11:30:13 +0530

    Update Day 5

---
+++ Day 5/postorder_stack2.cpp
@@ -0,0 +1,26 @@
+vector<int>postorder;
+        if(root==NULL)
+        return postorder;
+
+        stack<TreeNode *> st1, st2;
+        st1.push(root);
+
+        while(!st1.empty())
+        {
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+
+            if(root->left != NULL) st1.push(root->left);
+            
+            if(root->right !=NULL) st1.push(root->right);
+
+        }
+
+        while(!st2.empty())
+        {
+            TreeNode * node = st2.top();
+            st2.pop();
+            postorder.push_back(node->val);
+        }
+        return postorder;
