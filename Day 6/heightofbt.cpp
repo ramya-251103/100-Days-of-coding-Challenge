d00cb4e389ac5854ef0326e199dac10052a90a13
ramya-251103 <134849283+ramya-251103@users.noreply.github.com>
2024-06-10 03:37:03 +0530

    Create heightofbt.cpp

---
+++ Day 6/heightofbt.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+
+        if (root==NULL)
+        return 0;
+
+        int left_height = maxDepth(root->left);
+        int right_height = maxDepth(root->right);
+
+        return 1 + max(left_height, right_height);
+        
+    }
+};
