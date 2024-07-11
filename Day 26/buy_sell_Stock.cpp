a777bddd6102d322f7a5e163b3062c0f8ee1c824
Ramya Mishra <mishraramya251103@gmail.com>
2024-07-12 02:22:52 +0530

    Create buy_sell_Stock.cpp

---
+++ Day 26/buy_sell_Stock.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int mini = prices[0];
+
+        int maxProfit = 0;
+        int n = prices.size();
+
+        for(int i = 0 /*i =1 also*/; i<n ;i++)
+        {
+            int cost = prices[i] - mini;
+            maxProfit = max(maxProfit,cost);
+
+            mini = min(mini, prices[i]);
+        }
+
+        return maxProfit;
+        
+    }
+};
